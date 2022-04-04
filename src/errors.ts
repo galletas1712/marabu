61c8b970b7e102cf98dfc0aef0742b59f4e62815
Schwinn Saeree <17022745+galletas1712@users.noreply.github.com>
2022-04-03 18:14:17 -0700

    Initial commit

---
+++ src/errors.ts
@@ -0,0 +1,19 @@
+class InvalidMessageError extends Error {
+    constructor(msg?: string) {
+        if (typeof(msg) !== undefined) {
+            super(msg);
+        } else {
+            super("Message is in an invalid format");
+        }
+    }
+}
+
+class InvalidHelloMessageError extends InvalidMessageError {
+    constructor(msg?: string) {
+        if (typeof(msg) !== undefined) {
+            super(msg);
+        } else {
+            super("Hello message is in an invalid format");
+        }
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,116 @@
-console.log("Hello World");
+import * as net from "net";
+import { Message, HelloMessage } from "./interfaces";
+
+const requiredMessageKeys: { [key: string]: Array<string> } = {
+    "hello": ["type", "version"],
+    "error": ["type", "error"],
+    "getpeers": ["type"],
+    "peers": ["type", "peers"],
+    "getobject": ["type", "objectid"],
+    "ihaveobject": ["type", "objectid"],
+    "object": ["type", "object"],
+    "getmempool": ["type"],
+    "mempool": ["type", "txids"],
+};
+
+const optionalMessageKeys: { [key: string]: Array<string> } = {
+    "hello": ["agent"]
+};
+
+const PORT = 18080;
+const helloMessage = JSON.stringify({
+    "type": "hello",
+    "version": "0.8.0",
+    "agent": "Marabu-Core Client 0.8"
+});
+const getPeersMessage = JSON.stringify({"type": "getpeers"});
+const errorMessage = (errorMsg: string) => JSON.stringify({ "type": "error", "error": errorMsg });
+
+const onServerConnect = (socket: net.Socket) => {
+    socket.setEncoding("utf8");
+    socket.write(helloMessage);
+    socket.write(getPeersMessage);
+
+    let rcvMsgs: Array<Message> = [];
+    let buffer: string = "";
+    socket.on("data", (data: string) => {
+
+        //The regex seperates by newline, e.g. "helloworld\nilovejavascript" returns ["helloworld", "\n", "ilovejavascript"]
+        let dataArray: Array<String> = data.split(/(?=[\n])|(?<=[\n])/g);
+
+        for (let datum of dataArray) {
+            buffer += datum;
+
+            if (datum === "\n") {
+                try {
+                    const currMsg = parseAndValidateBuffer(buffer);
+                    handleMessage(currMsg, rcvMsgs, socket);
+                    rcvMsgs.push(currMsg);
+                } catch (err) {
+                    if (err instanceof Error) {
+                        socket.write(errorMessage(err.message));
+                    }
+                    if (err instanceof InvalidHelloMessageError) {
+                        socket.end();
+                    }
+                } finally {
+                    buffer = "";
+                }
+            }
+        }
+    });
+
+    socket.on("end", () => { });
+
+    socket.on("error", () => { });
+};
+
+const parseAndValidateBuffer = (buffer: string): Message => {
+    const deserialized: Object = JSON.parse(buffer);
+    if (!deserialized.hasOwnProperty("type")) {
+        throw new InvalidMessageError;
+    }
+    const result = deserialized as Message;
+    let foundError : boolean = false;
+    for (const key in Object.keys(result)) {
+        if (!(key in requiredMessageKeys[result.type]) && !(!(result.type in optionalMessageKeys) || key in optionalMessageKeys[result.type])) {
+            foundError = true;
+            break;
+        }
+    }
+    for (const key in requiredMessageKeys[result.type]) {
+        if(!(key in result)) {
+            foundError = true;
+            break;
+        }
+    }
+    if (foundError) {
+        if (result.type === "hello") {
+            throw new InvalidHelloMessageError;
+        } else {
+            throw new InvalidMessageError;
+        }
+    }
+    return result;
+}
+
+const handleMessage = (currMsg: Message, prevMsgs: Array<Message>, socket: net.Socket) => {
+    //validate that first message is a valid hello
+    if (prevMsgs.length === 0) {
+        //if first message isn't hello 
+        if (currMsg["type"] !== "hello") {
+            throw new InvalidHelloMessageError("First message is not hello");
+        }
+        let versionTest = new RegExp("0\.8\..");
+        //if version doesn't exist or version is invalid value, close socket
+        if (currMsg["type"] === "hello" && versionTest.test((currMsg as HelloMessage)["version"]) === false) {
+            throw new InvalidHelloMessageError;
+        }
+    }
+
+    //validate getPeers; need to write known peers to text file
+};
+
+const server = net.createServer();
+server.listen(PORT);
+server.on("connection", onServerConnect);
