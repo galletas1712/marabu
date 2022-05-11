ad35f04077928b43391d2167d04799e86c314a19
KachachanC <56240542+KachachanC@users.noreply.github.com>
2022-05-11 05:45:14 -0700

    revert config changes

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,9 @@
 import { Block } from "./types/transactions";
 
-export const PEERS_DB_PATH = "./DB/peers.list";
-export const OBJECT_DB_PATH = "./DB/objects.list";
-export const UTXO_DB_PATH = "./DB/utxo.list";
-export const BLOCK_HEIGHT_DB_PATH = "./DB/block_height_db.list";
+export const PEERS_DB_PATH = "./peers.list3";
+export const OBJECT_DB_PATH = "./objects.list3";
+export const UTXO_DB_PATH = "./utxo.list3";
+export const BLOCK_HEIGHT_DB_PATH = "./Dblock_height_db.list3";
 export const CURRENT_VERSION = "0.8.0";
 export const AGENT = "<Kachachan + Schwinn> Marabu Client 0.8";
 export const ACCEPTABLE_VERSIONS = "0.8.x";
