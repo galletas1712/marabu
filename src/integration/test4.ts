a35227e0ff01cb4c33a5b04095fdadc3ad03aec2
KachachanC <56240542+KachachanC@users.noreply.github.com>
2022-05-17 15:29:59 -0700

    Update test4 with staff test cases

--- src/integration/test4.ts
+++ src/integration/test4.ts
@@ -162,7 +162,68 @@ async function test5(){
 }
 
 
-const testsArray = [test0];
+async function sendTxnNeeded(){
+    let obj1 = `{"object":{"height":1,"outputs":[{"pubkey":"e3b1f3cd0b79e9ec9c4adcb38ac147c14bfb4184941af70ab6debefcb752445f","value":50000000000000}],"type":"transaction"},"type":"object"}` + '\n';
+    let obj2 = `{"object":{"height":2,"outputs":[{"pubkey":"e3b1f3cd0b79e9ec9c4adcb38ac147c14bfb4184941af70ab6debefcb752445f","value":60000000000000}],"type":"transaction"},"type":"object"}` + '\n';
+    let obj3 = `{"object":{"height":3,"outputs":[{"pubkey":"e3b1f3cd0b79e9ec9c4adcb38ac147c14bfb4184941af70ab6debefcb752445f","value":80000000000000}],"type":"transaction"},"type":"object"}` + '\n';
+    let tx1 = `{"object":{"inputs":[{"outpoint":{"index":0,"txid":"8f75e875d371a0970602550cf0da63dc07ce05331929c2b46f080582cce42dfb"},"sig":"a589fae78880c015e0d30b176fca6461ff1a8a0932031d9e0256f7879878dd344d0a2b4365d5a71dfad51b1985afc44c54c894c6d3013102f91264854e5b9d00"}],"outputs":[{"pubkey":"e3b1f3cd0b79e9ec9c4adcb38ac147c14bfb4184941af70ab6debefcb752445f","value":40000000000000}],"type":"transaction"},"type":"object"}` + '\n';
+    let tx2 = `{"object":{"inputs":[{"outpoint":{"index":0,"txid":"bee2e10d1e415525d4ff1756ecb88f9579c58de7560020ecfb5fd7cd8abced12"},"sig":"3a4f921b20138275de88f9d621246aa6dc8bc3f17eafebb4373301aab8ee93e688a34e497f9ced702c1e9ac73a83054f1f1a130cdf8b76472e0164bb9e175a0b"}],"outputs":[{"pubkey":"e3b1f3cd0b79e9ec9c4adcb38ac147c14bfb4184941af70ab6debefcb752445f","value":30000000000000}],"type":"transaction"},"type":"object"}` + '\n';
+    let obj4 = `{"object":{"height":1,"outputs":[{"pubkey":"d955ceeb458058b5e544de996543d08aff42ff01b386e6439c0a427488785bf5","value":50000000000000}],"type":"transaction"},"type":"object"}` + '\n';
+    let obj5 = `{"object":{"height":2,"outputs":[{"pubkey":"d955ceeb458058b5e544de996543d08aff42ff01b386e6439c0a427488785bf5","value":50000000000000}],"type":"transaction"},"type":"object"}` + '\n';
+    let obj6 = `{"object":{"height":4,"outputs":[{"pubkey":"d955ceeb458058b5e544de996543d08aff42ff01b386e6439c0a427488785bf5","value":50000000000000}],"type":"transaction"},"type":"object"}` + '\n';
+
+    createNewClient([helloMsg, obj1, obj2, obj3, tx1, tx2, obj4, obj5, obj6]);
+}
+
+async function testStaff1(){
+    console.log(
+        "Test Staff 1: Expecting Invalid Coinbase"
+    );
+    let block1 = `{"object":{"T":"00000002af000000000000000000000000000000000000000000000000000000","created":1652763875,"miner":"grader","nonce":"0000000000000000000000000000000000000000000000000000000002fa207e","note":"First block","previd":"00000000a420b7cefa2b7730243316921ed59ffe836e111ca3801f82a4f5360e","txids":["1afc7e763bd235919405a04fff70e2e0fcdabfdfa933102b93e8bd8666179b69"],"type":"block"},"type":"object"}` + '\n';
+    let block2 = `{"object":{"T":"00000002af000000000000000000000000000000000000000000000000000000","created":1652763890,"miner":"grader","nonce":"000000000000000000000000000000000000000000000000000000000350e963","note":"Second block","previd":"00000000fc0460609d50414768b9f891e007b13e38f09d4074ff283ef6b88f74","txids":["185e8e44563747ceaa7f8f14c627631aaa4829ea1b8ffbf39a365f49114e6334"],"type":"block"},"type":"object"}` + '\n';
+    let block3 = `{"object":{"T":"00000002af000000000000000000000000000000000000000000000000000000","created":1652763907,"miner":"grader","nonce":"00000000000000000000000000000000000000000000000000000000c1dd9580","note":"Third block","previd":"00000001a5b4c41f0f176d63e9dd0f3179dda475089fea8f4f2277c27052037f","txids":["e38fc71a0d04e985a4f4d16e37aa8a151e06c93710a31fb127794b3a8700422e"],"type":"block"},"type":"object"}` + '\n';
+    
+    createNewClient([helloMsg, block1, block2, block3])
+}
+
+async function testStaff2(){
+    console.log(
+        "Test Staff 2: Expecting Non-increasing timestamp"
+    );
+    let block1 = `{"object":{"T":"00000002af000000000000000000000000000000000000000000000000000000","created":1652765373,"miner":"grader","nonce":"000000000000000000000000000000000000000000000000000000002e2dcd5a","note":"First block","previd":"00000000a420b7cefa2b7730243316921ed59ffe836e111ca3801f82a4f5360e","txids":["8f75e875d371a0970602550cf0da63dc07ce05331929c2b46f080582cce42dfb"],"type":"block"},"type":"object"}` + '\n';
+    let block2 = `{"object":{"T":"00000002af000000000000000000000000000000000000000000000000000000","created":1652765373,"miner":"grader","nonce":"0000000000000000000000000000000000000000000000000000000001187622","note":"Second block","previd":"000000021b3ea6efe336d968b9ee7143dc7e80280b582a13fa45fcb7f5cc6e29","txids":["bee2e10d1e415525d4ff1756ecb88f9579c58de7560020ecfb5fd7cd8abced12","20ec9aecf9cd3603b037414b8d5cf72ece7df4dc88d0beef4f8add2569c2b78c"],"type":"block"},"type":"object"}` + '\n';
+    let block3 = `{"object":{"T":"00000002af000000000000000000000000000000000000000000000000000000","created":1652765373,"miner":"grader","nonce":"000000000000000000000000000000000000000000000000000000019066a58e","note":"Third block","previd":"000000026537a79d27665ac3f8cb9bfd446866781b6043e0e7f7e016e4060daf","txids":["1c3c877640c81a0b9a9baccadeb4bff674ea520ecb9e5a1945bf22f7c0e0399a","84a53fe7b6f3d51e10940abe99c2621b3aee18e2ea9fbec3a50f7068f7932018"],"type":"block"},"type":"object"}` + '\n';
+    
+    createNewClient([helloMsg, block1, block2, block3])
+}
+
+async function testStaff3(){
+    console.log(
+        "Test Staff 3: Expecting Future Timestamps"
+    );
+    let block1 = `{"object":{"T":"00000002af000000000000000000000000000000000000000000000000000000","created":1652811200,"miner":"grader","nonce":"000000000000000000000000000000000000000000000000000000004068b034","note":"First block","previd":"00000000a420b7cefa2b7730243316921ed59ffe836e111ca3801f82a4f5360e","txids":["8f75e875d371a0970602550cf0da63dc07ce05331929c2b46f080582cce42dfb"],"type":"block"},"type":"object"}` + '\n';
+    let block2 = `{"object":{"T":"00000002af000000000000000000000000000000000000000000000000000000","created":1652811557,"miner":"grader","nonce":"000000000000000000000000000000000000000000000000000000007d953bbf","note":"Second block","previd":"000000022ba6f209982e3c06750355c1145170165c6e568df5e1c07b760aaa40","txids":["bee2e10d1e415525d4ff1756ecb88f9579c58de7560020ecfb5fd7cd8abced12","20ec9aecf9cd3603b037414b8d5cf72ece7df4dc88d0beef4f8add2569c2b78c"],"type":"block"},"type":"object"}` + '\n';
+    let block3 = `{"object":{"T":"00000002af000000000000000000000000000000000000000000000000000000","created":9950650399,"miner":"grader","nonce":"0000000000000000000000000000000000000000000000000000000000f72837","note":"Third block","previd":"000000019c2abb91d400192d9e3893ceb91a11b9e2ce631889ab18224f61fb77","txids":["1c3c877640c81a0b9a9baccadeb4bff674ea520ecb9e5a1945bf22f7c0e0399a","84a53fe7b6f3d51e10940abe99c2621b3aee18e2ea9fbec3a50f7068f7932018"],"type":"block"},"type":"object"}` + '\n';
+    
+    createNewClient([helloMsg, block1, block2, block3])
+}
+
+
+async function testStaff4(){
+    console.log(
+        "Test Staff 4: Expecting Incorrect Genesis"
+    );
+    let block1 = `{"object":{"T":"00000002af000000000000000000000000000000000000000000000000000000","created":1652815651,"miner":"grader","nonce":"00000000000000000000000000000000000000000000000000000000285de90c","note":"Incorrect genesis","previd":null,"txids":[],"type":"block"},"type":"object"}` + '\n';
+    let block2 = `{"object":{"T":"00000002af000000000000000000000000000000000000000000000000000000","created":1652815834,"miner":"grader","nonce":"0000000000000000000000000000000000000000000000000000000058d875c1","note":"First block","previd":"000000029817ed79954f155c02163babe677b7a5115293f82887024f4ba648ba","txids":[],"type":"block"},"type":"object"}` + '\n';
+    let block3 = `{"object":{"T":"00000002af000000000000000000000000000000000000000000000000000000","created":1652816236,"miner":"grader","nonce":"0000000000000000000000000000000000000000000000000000000013450c8b","note":"Second block","previd":"000000026c655cdc4e9c7ccd91486b7c3400380085304cb893bfb17475841d46","txids":[],"type":"block"},"type":"object"}` + '\n';
+    
+    createNewClient([helloMsg, block1, block2, block3])
+}
+
+
+const testsArray = [sendTxnNeeded, testStaff1, testStaff2, testStaff3, testStaff4];
+
+//const testsArray = [test0, test1, test2, test3, test4, test5, sendTxnNeeded, testStaff1, testStaff2, testStaff3, testStaff4];
 
 async function tests() {
   console.log("------------------------------------------------");
